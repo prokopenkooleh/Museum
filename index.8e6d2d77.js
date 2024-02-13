9f9196dd9b18a4b4e3196c148380495ef073927a
Oleh Prokopenko <olehprokopenko1@gmail.com>
2024-02-13 18:16:08 +0200

    make build

---
+++ index.8e6d2d77.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars, no-undef
+new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:16,centeredSlides:!0,pagination:{el:".swiper-pagination",clickable:!0},width:280,breakpoints:{768:{slidesPerView:1,spaceBetween:20,width:340}}});var e=document.getElementById("link");window.screen.availWidth>=1280&&(e.href="#gallery");//# sourceMappingURL=index.8e6d2d77.js.map
+
+//# sourceMappingURL=index.8e6d2d77.js.map
